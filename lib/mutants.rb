75f92cf9d7d60d6c946c1a822f4b74c4431b649a
jesus <jesus.alberto.vk@gmail.com>
2022-03-18 23:53:05 -0600

    First commit

---
+++ lib/mutants.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mutants/version"
+
+module Mutants
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mutants/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Mutants
+  VERSION = "0.1.0"
+end
