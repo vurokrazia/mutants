32219f3fe43fc4e030eac105a3976879863b613f
jesus <jesus.alberto.vk@gmail.com>
2022-03-18 23:54:50 -0600

    Update dependencies

--- spec/mutants_spec.rb
+++ spec/mutants_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Mutants do
     expect(Mutants::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  # it "does something useful" do
+  #   expect(false).to eq(true)
+  # end
 end
