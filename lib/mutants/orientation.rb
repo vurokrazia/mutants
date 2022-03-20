0dce4d581df7718a522ceb4d36a8aaa1c9afaa3a
jesus <jesus.alberto.vk@gmail.com>
2022-03-20 02:35:33 -0600

    Add constants to positions to read

---
+++ lib/mutants/orientation.rb
@@ -0,0 +1,10 @@
+module Mutants
+    module Orientation
+        HORIZONTAL = 0
+        VERTICAL = 1
+        BELOW_MAIN_DIAGONAL = 2
+        ABOVE_MAIN_DIAGONAL_INCLUDING = 3
+        FROM_RIGHT = 4
+        FROM_LEFT = 5
+    end
+end
