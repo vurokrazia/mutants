50a23e83197dc9860a1154184db0a34a8b836200
jesus <jesus.alberto.vk@gmail.com>
2022-03-21 15:22:35 -0600

    remove doble comma

--- lib/mutants.rb
+++ lib/mutants.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-require_relative "mutants/version"
+require_relative 'mutants/version'
 
-require "mutants/configuration"
-require "mutants/orientation"
-require "mutants/dna"
+require 'mutants/configuration'
+require 'mutants/orientation'
+require 'mutants/validations'
+require 'mutants/detector'
+require 'mutants/dna'
